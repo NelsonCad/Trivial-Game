dccafab884026166a0cd4d95f96aee61ffae595e
NelsonCad <NelsonCad97@gmail.com>
2019-07-24 16:38:24 -0700

    question and timer shows on page on start

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,46 +1,86 @@
-const trivia = [
-    {
-        quest: "radarada",
-        ans1: "chef",
-        ans2: "apprentice",
-        ans3: "assistant",
-        ans4: "nusiance"
-    },
-    {
-        quest: "Beta leader",
-        ans1: "John",
-        ans2: "Rose",
-        ans3: "Jade",
-        ans4: "Dave"
-    },
-    {
-        quest: "Alpha Leader",
-        ans1: "Jane",
-        ans2: "Dirk",
-        ans3: "Roxy",
-        ans4: "Jake"
-    },
-    {
-        quest: "midnight crew leader",
-        ans1: "daimonds droog",
-        ans2: "hearts boxcar",
-        ans3: "clubs deuce",
-        ans4: "spades slick"
-    },
-    {
-        quest: "life meaning",
-        ans1: "42",
-        ans2: "self-happiness",
-        ans3: "respect from peers",
-        ans4: "alpha timeline"
-    }];
-
-const trivAns = ["assistant", "John", "Dirk", "spades slick", "42"];
-
-let triviaQues = 0;
-
-//time set per question
-let time = 45;
+$(document).ready(function () {
 
 
-$(".start").
+    const trivia = [
+        {
+            quest: "radarada",
+            answers: {
+                ans1: "chef",
+                ans2: "apprentice",
+                ans3: "assistant",
+                ans4: "nusiance"
+            }
+        },
+        {
+            quest: "Beta leader",
+            answers: {
+                ans1: "John",
+                ans2: "Rose",
+                ans3: "Jade",
+                ans4: "Dave"
+            }
+        },
+        {
+            quest: "Alpha Leader",
+            answers: {
+                ans1: "Jane",
+                ans2: "Dirk",
+                ans3: "Roxy",
+                ans4: "Jake"
+            }
+        },
+        {
+            quest: "midnight crew leader",
+            answers: {
+                ans1: "daimonds droog",
+                ans2: "hearts boxcar",
+                ans3: "clubs deuce",
+                ans4: "spades slick"
+            }
+        },
+        {
+            quest: "life meaning",
+            answers: {
+                ans1: "42",
+                ans2: "self-happiness",
+                ans3: "respect from peers",
+                ans4: "alpha timeline"
+            }
+        }];
+
+    const trivAns = ["assistant", "John", "Dirk", "spades slick", "42"];
+
+    $("#start").on("click", startTriv);
+    let triviaI = 0;
+
+    //time set per question
+    let time = 45;
+    let interValid;
+
+    function timer() {
+        time--;
+        $(".timer").text(time);
+    }
+
+    // start of the trivia game
+    function startTriv() {
+
+        // sets timer for question
+        clearInterval(interValid);
+        interValid = setInterval(timer, 1000);
+
+        //creates h1 with the question in the correct box
+        let TriviaQuest = $("<h3>").text(trivia[triviaI].quest);
+
+        
+
+        
+        
+        $(".questions").append(TriviaQuest);
+
+
+
+    }
+
+
+});
