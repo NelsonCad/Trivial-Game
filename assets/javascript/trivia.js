efe3f095e708e2749c3749fec7c7538b0ae2fc41
NelsonCad <NelsonCad97@gmail.com>
2019-07-24 09:50:28 -0700

    added base global values

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,46 @@
+const trivia = [
+    {
+        quest: "radarada",
+        ans1: "chef",
+        ans2: "apprentice",
+        ans3: "assistant",
+        ans4: "nusiance"
+    },
+    {
+        quest: "Beta leader",
+        ans1: "John",
+        ans2: "Rose",
+        ans3: "Jade",
+        ans4: "Dave"
+    },
+    {
+        quest: "Alpha Leader",
+        ans1: "Jane",
+        ans2: "Dirk",
+        ans3: "Roxy",
+        ans4: "Jake"
+    },
+    {
+        quest: "midnight crew leader",
+        ans1: "daimonds droog",
+        ans2: "hearts boxcar",
+        ans3: "clubs deuce",
+        ans4: "spades slick"
+    },
+    {
+        quest: "life meaning",
+        ans1: "42",
+        ans2: "self-happiness",
+        ans3: "respect from peers",
+        ans4: "alpha timeline"
+    }];
+
+const trivAns = ["assistant", "John", "Dirk", "spades slick", "42"];
+
+let triviaQues = 0;
+
+//time set per question
+let time = 45;
+
+
+$(".start").
